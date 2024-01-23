d5ec2adbd2de9f3ced1bb69696c18eb3456d2ed6
StasProzorov11 <stasprozorov11@gmail.com>
2024-01-23 18:35:31 +0100

    amendments

--- lesson1/main.py
+++ lesson1/main.py
@@ -1,10 +1,1 @@
-class Person:
-    def __init__(self, name: str, age: int) -> None:
-        self.name: str = name
-        self.age: int = age
-
-
-class Price:
-    def __init__(self, currency: str, value: int) -> None:
-        self.currency: str = currency
-        self.value: int = value
+print("hi")
